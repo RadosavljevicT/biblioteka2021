57984526253c985b3476bfe9954aed4ec319f880
tamararadosavljevic <radosavljevic.tamara97@gmail.com>
2021-03-08 12:54:34 +0100

    Implementirana logicka kontrola u klasi Biblioteka

--- src/biblioteka/Biblioteka.java
+++ src/biblioteka/Biblioteka.java
@@ -1,7 +1,39 @@
 package biblioteka;
 
+import java.util.LinkedList;
+
 import biblioteka.interfejs.BibliotekaInterface;
 
 public class Biblioteka implements BibliotekaInterface {
+	
+	private LinkedList <Knjiga> knjige = new LinkedList<Knjiga>();
 
+
+	 public void dodajKnjigu (Knjiga knjiga) {
+		
+		 if (knjiga == null || knjige.contains(knjiga))
+			 throw new RuntimeException ("Knjiga ne sme biti null i ne sme biti duplikat"); 
+		 
+		 knjige.add(knjiga);
+		
+	}
+	public void obrisiKnjigu (Knjiga knjiga) {
+		
+		if (knjiga == null || !knjige.contains(knjiga))
+		throw new RuntimeException ("Knjiga ne sme biti null i mora postojati u biblioteci");
+		knjige.remove(knjiga);
+		
+	}
+	
+	public LinkedList <Knjiga> vratiSveKnjige (){
+		
+		return knjige;
+		
+	}
+	
+	
+		
+		
+	
 }
+
